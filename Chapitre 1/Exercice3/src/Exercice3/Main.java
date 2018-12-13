25bf42a0b6599e360100d65bc9eb87f77a29e6d9
Nathanaël NEZONDET <nat.nez.ren@gmail.com>
2018-12-13 03:32:56 +0100

    Exercice3 commit
    On commit le 3ième exercice, quelques difficulté avec l'utilisation des Exception

---
+++ Chapitre 1/Exercice3/src/Exercice3/Main.java
@@ -0,0 +1,30 @@
+package Exercice3;
+
+import java.text.ParseException;
+
+public class Main {
+    public static void main(String[] args){
+        String [] array = {"1413","341","13","3151","131","666"};
+        int result;
+
+        result = calculateAverage(array);
+
+        System.out.println("result : " + result);
+
+    }
+
+    public static int calculateAverage(String [] array){
+        int average = 0;
+
+        for (String element:array){
+            try {
+                average += Integer.parseInt(element);
+            }catch (ParseException e) {
+                System.out.println(element + " is not a interger");
+                System.out.println("Error : " + e.getMessage());
+            }
+        }
+
+        return average/array.length;
+    }
+}
