412ade2fbf2087050b44a8c6f64734c990373d61
Nathanaël NEZONDET <nat.nez.ren@gmail.com>
2018-12-13 12:37:43 +0100

    MAJ Ex3

--- Chapitre 1/Exercice3/src/Exercice3/Main.java
+++ Chapitre 1/Exercice3/src/Exercice3/Main.java
@@ -1,11 +1,11 @@
-package Exercice3;
+package Exercice3;//jamais de chiffre dans les packages!!!!!!!!!!!!!!!!!!!!
 
 import java.text.ParseException;
 
 public class Main {
     public static void main(String[] args){
         String [] array = {"1413","341","13","3151","131","666"};
-        int result = 0;
+        double result = 0;
 
         try {
             result = calculateAverage(array);
@@ -17,19 +17,16 @@ public class Main {
 
     }
 
-    public static int calculateAverage(String [] array) throws ParseException {
-        int average = 0;
+    public static double calculateAverage(String [] array) throws ParseException {
+        double average = 0;
 
         for (String element:array){
             average += Integer.parseInt(element);
         }
 
-        try{
-            average/=array.length;
-        }catch (RuntimeException e){
-            System.out.println(e);
+        if(array.length != 0){
+            return average/array.length;
         }
-
         return average;
     }
 
